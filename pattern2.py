076fce3e21e2cddab8a87ed0e63bab39da957449
Ananya B M <ananyabm99@gmail.com>
2025-10-04 23:08:42 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,133 @@
+print("1")
+n=5
+for i in range (n):
+    for j in range  (n):
+        print("*",end = " ")
+    print()
+print(" "*100)   
+
+print("2")
+n=6
+for i in range(n):
+    if n>=i:
+        print("*"*n)
+        i+=1
+print()  
+print(" "*100)    
+          
+print("3")    
+n=6
+for i in range (n):
+    if i <= n:
+        print(" * "*i)
+print()
+print(" "*100)        
+
+print("4")
+n= 6
+for i in range (n):
+    for j in range (i+1): 
+        print("*",end=" ")  
+    print()   
+print(" "*100)        
+    
+print("5")
+n=6
+for i in range (n):
+    for j in range (i,n) :
+        print("*",end = " ")
+    print() 
+print(" "*100)     
+
+print("6")    
+n=5
+for i in range(n):
+    for j in range (i,n):
+        print(" ",end= " ")
+    for j in range (i+1) :
+        print("*",end = " ") 
+    print() 
+print(" "*100) 
+
+print("7")         
+n=5
+for i in range(n)    :
+    for j in range(i+1):
+        print("*",end = " ")
+    for j in range(i,n)    :
+        print(" ",end=" ")
+    print() 
+print(" "*100)        
+
+print("8")    
+n=5
+for i in range (n)    :
+    for j in range (i+1):
+        print(" ",end=" ")
+    for j in range (i,n)    :
+        print("*",end=" ")
+    print() 
+print(" "*100)       
+
+print("9")
+n=5
+for i in range (n) :
+    for j in range (i,n):
+        print(" ",end=" ")
+    for j in range (i)    :
+        print("*",end = " ")
+    for j in range (i+1)    :
+        print("*",end =" ")
+    print()
+print(" "*100)        
+
+print("10")
+n=5
+for i in range (n):
+    for j in range (i+1):
+        print(" ",end = "  ")
+    for j in range (i,n-1)    :
+        print("*",end = "  ")
+    for j in range (i,n)    :
+        print("*",end = "  ")
+    print()
+print(" "*100)       
+
+print("11") 
+n=5
+for i in range (n):
+    for j in range (i+1):
+        print(" ",end = "  ")
+    for j in range (i)    :
+        print("*",end = "  ")
+    for j in range (i,n)    :
+        print("*",end = "  ")
+    print() 
+print(" "*100)
+
+print("12")
+n=5    
+for i in range (n-1) :
+    for j in range (i,n):
+        print(" ",end=" ")
+    for j in range (i)    :
+        print("*",end = " ")
+    for j in range (i+1)    :
+        print("*",end =" ")
+    print()  
+for i in range(n)       : 
+    for j in range (i+1):
+        print(" ",end=" ")
+    for j in range (i,n-1)    :
+        print("*",end = " ")
+    for j in range (i,n)    :
+        print("*",end = " ")
+    print()    
+print(" "*100)        
+            
+        
+    
+
+        
+    
+    
