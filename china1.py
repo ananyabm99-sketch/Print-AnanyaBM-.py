41e078dc75bed62d3e6de41c703198645a9e2341
Ananya B M <ananyabm99@gmail.com>
2025-10-08 23:43:01 +0530

    Add files via upload

---
+++ china1.py
@@ -0,0 +1,10 @@
+
+print(len(input("Enter ur name :")))
+#large integers
+print(123_456_789)
+name = input("Enter your name : ")
+length = len(name)
+print("The number of letters in your name is "+ str(length))
+r = (4456783/2**2)
+print(round(r,2))
+print(type(2**2))
