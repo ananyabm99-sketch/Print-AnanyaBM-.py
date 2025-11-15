a6dc907aa0a2d696fde66be0927b95cbe2bdd1ae
Ananya B M <ananyabm99@gmail.com>
2025-11-15 23:54:36 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,61 @@
+#Program to find area and perimeter of a rectangle
+'''length = float(input("Enter the length of the rectangle : "))
+width = float(input("Enter the width of a rectangle : "))
+area = length * width
+print("Area of rectangle is : ",area)
+perimeter = 2 *(length + width)
+print('Area of perimeter is : ',perimeter)'''
+
+#INTEREST CALCULATION
+'''p = float(input("Enter the principle amount: "))
+t = float(input("Enter the time period of amount: "))
+r = float(input("Enter the rate of interest: "))
+simple_interest = (p*t*r)/100
+print(simple_interest)
+
+message = "  H a i  "
+print(message.upper())
+print(message.lower())
+print(message.strip()* 2)
+print(message.replace(" H a i "," Ananya "))
+print(message.replace (" H a i "," poorvi "))
+print(message)
+
+a = "mam successfully completed the class"
+print(len(a))
+
+name = "Ananya"
+print(name[5])
+print(name[1:4:2])
+n = "Poochandana"
+print(n[3:12:2])
+print(n[-4])
+print(n[::2])
+
+#Lists  in  python
+item1 = "Milk"
+item2 = "Sugar"
+item3 = "Bru"
+print(item1,item2,item3)'''
+
+l = ["Bru","Bred","coffee","Pen","Bat"]
+print(l[0:5])
+print(l.pop())
+print(l.pop(1))
+print(l)
+a = l.append("Coat")
+r = l.remove("Bru")
+print(l.insert(1,'Cad'))
+print(l)
+l[1] = "Manaswi"
+print(l)
+print(len(l))
+
+items = [1,2,11,3,5,4]
+print(sorted(items))
+print(sum(items))
+items.sort()
+print(items)
+
+
+    
