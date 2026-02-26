b8f1f8e8431e817f1d2b85e8b449971339edf4a1
Ananya B M <ananyabm99@gmail.com>
2026-02-26 23:48:42 +0530

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,23 @@
+'''def welcome (func):
+    def wrapper():
+        print("Welcome to PESC Mandya")
+        func()
+        print("Thank you ,have a nice day!")
+    return wrapper
+
+@welcome
+def Bhashana():
+    print("PESC Mandya had a History of 62 years.Dr Shankaregowda is the founder of PESC Mandya")
+Bhashana()
+'''
+#passing arguments to the decorator
+def welcome (func):
+    def wrapper(name):
+        print("Good Morning")
+        func(name)
+        print("Have a Nice Day!")
+    return wrapper
+@welcome
+def std_name(name):
+    print("My name is ",name)
+std_name("Ananya B M")
