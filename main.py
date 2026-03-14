521ab0c242256eb2ab8742ee009bcecd5b63134a
Ananya B M <ananyabm99@gmail.com>
2026-03-14 21:46:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-import pandas
-data = pandas.read_csv("2018_Central_park_Squirrel_Census_-_Squirrel_Data_20260308.csv")
 
-
-gray_sqrl_count = len(data[data["Primary Fur Color"]=="Gray"])
-red_sqrl_count = len(data[data["Primary Fur Color"]=="Cinnamon"])
-black_sqrl_count = len(data[data["Primary Fur Color"]=="Black"])
-color =[]
-color.append(gray_sqrl_count)
-color.append(red_sqrl_count)
-color.append(black_sqrl_count)
-
-
-'''print(gray_sqrl_count)
-print(red_sqrl_count)
-print(black_sqrl_count)
-colour = ["Gray","Cinnamon","Black"]
-sqrl_c = {}
-for index,colour in enumerate(colour):
-    sqrl_c[colour]=color[index]
-print(sqrl_c) 
+#list comprehension
+numbers = [1,2,3]
+'''for n in numbers:
+    add_1 = n+1
+    new_list.append(add_1)
+print(new_list)
 '''
+#List Comprehension:   
+new_list = [n+1 for n in numbers]
+print(new_list)
+
+'''name = "Ananya"
+#new_list = [letter for letter in name]
+#print(new_list)
+
+new_list = [letter*2 for letter in name]
+print(new_list)
+
+new_list = [letter+letter for letter in name]  #both are same
+print(new_list)
 
 
-data_dict = {
-    "Fur Color":["Gray","Cinnamon","Black"],
-    "Count":[2473,392,103]
-}
-df=pandas.DataFrame(data_dict)
-print(df)
-df.to_csv("Squrrel_count.csv")
+#range
+list_item = []
+for i in range(1,5):
+    list_item.append(i)
+print(list_item)  
+new_list = [n*2 for n in range(1,5)] 
+print(new_list)
+''' 
+#new_list = [new_list for number in list if test]
+'''new_list = [n*2 for n in range(1,5)] 
+print(new_list)
+'''
+d = ["Anu","Bhagya","Mahadev","Anvith",""]
+print(d)
+new_list = [name for name in d if len(name)<=4]
+print(new_list)
+
+cap_list = [name.upper() for name in d if len(name)>=5]
+print(cap_list)
 
